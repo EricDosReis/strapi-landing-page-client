610ade0fc3a63b4b922c0659b8477bd1728043db
ericsantos <eric@exactaworks.com.br>
2020-11-16 13:04:53 -0300

    feat: fetching header data

--- src/types/api.ts
+++ src/types/api.ts
@@ -3,6 +3,24 @@ export type LogoProps = {
   url: string
 }
 
+export type ButtonProps = {
+  label: string
+  url: string
+}
+
+export type ImageProps = {
+  alternativeText: string
+  url: string
+}
+
+export type HeaderProps = {
+  title: string
+  description: string
+  button: ButtonProps
+  image: ImageProps
+}
+
 export type LandingPageProps = {
   logo: LogoProps
+  header: HeaderProps
 }
